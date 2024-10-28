63fe1bc18a689524a0b923f020b5b4217a6076cd
JibyJo <165126613+JibyJo@users.noreply.github.com>
2024-10-28 11:15:58 +0530

    Update model.jsx

--- src/threescene/model.jsx
+++ src/threescene/model.jsx
@@ -1,6 +1,7 @@
 import { useGLTF } from '@react-three/drei';
 
 export const Model = () => {
-  const { scene } = useGLTF('/models/realistic_palm_tree_free.glb');
+  const { scene } = useGLTF('https://JibyJo.github.io/react3D/models/realistic_palm_tree_free.glb
+');
   return <primitive object={scene} scale={0.5} position={[0, 0, 0]} />;
 };
