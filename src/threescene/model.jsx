bd6dfd110be2fe238a314100ea4dcdc241a189ff
JibyJo <165126613+JibyJo@users.noreply.github.com>
2024-10-28 11:54:41 +0530

    Delete src directory

--- src/threescene/model.jsx
+++
@@ -1,7 +0,0 @@
-import { useGLTF } from '@react-three/drei';
-
-export const Model = () => {
-  const { scene } = useGLTF('https://JibyJo.github.io/react3D/models/realistic_palm_tree_free.glb
-');
-  return <primitive object={scene} scale={0.5} position={[0, 0, 0]} />;
-};
